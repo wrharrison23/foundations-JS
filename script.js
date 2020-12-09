20526c8e68fd4b4e37cfe4b192f1ad001005b6d3
Wes <wrharrison23@gmail.com>
2020-12-08 19:47:45 -0500

    Day 2 finished code

---
+++ script.js
@@ -0,0 +1,27 @@
+//alert("Hello, world")
+
+console.log("Howdy, world!")
+
+// let firstName = 'Wes'
+// console.log(firstName)
+// let lastName = 'Harrison'
+// console.log(lastName)
+// let age = 24
+// console.log(age)
+// let likesTravel = true
+// console.log(likesTravel)
+
+let wesObject = {
+    firstName: "Wes",
+    lastName: 'Harrison',
+    age: 24,
+    likesTravel: true,
+    favoriteMovies: ['Talladega Nights', 'Godfather'],
+}
+
+console.log(wesObject)
+
+
+// let movieArray = ['Talladega Nights', 'Godfather']
+// console.log(movieArray[0])
+// console.log(movieArray[1])
