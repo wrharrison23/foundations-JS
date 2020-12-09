12742a8e0365b644c52a84baf30a534334d3d6e3
wrharrison23 <60231606+wrharrison23@users.noreply.github.com>
2020-12-09 18:57:26 -0500

    Add files via upload

---
+++ conditionals/script.js
@@ -0,0 +1,13 @@
+let likesToTravel = true
+if (likesToTravel === true){
+    console.log('Bon voyage!')
+} else {
+    console.log('Enjoy your couch!')
+} // should be 'Bon voyage!'
+
+let favoriteFood = 'Fried Chicken'
+if (favoriteFood === 'fried pickles' || favoriteFood == 'pepperoni rolls'){
+    console.log('Congratulations! You have excellent taste!')
+} else {
+    console.log('Clearly you have not tried fried pickles or pepperoni rolls.')
+}
